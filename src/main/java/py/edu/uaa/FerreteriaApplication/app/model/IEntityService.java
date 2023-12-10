701b1fcb8dde07ebfd2f8d27b3e3c06ae85774b7
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-12-10 12:57:06 -0300

    Dependencia swagger en pom.xml

---
+++ src/main/java/py/edu/uaa/FerreteriaApplication/app/model/IEntityService.java
@@ -0,0 +1,15 @@
+package py.edu.uaa.FerreteriaApplication.app.model;
+
+import java.util.List;
+
+public interface IEntityService<T, ID> {
+    List<T> findAll();
+
+    T findById(ID id);
+
+    T create(T entity);
+
+    T update(T entity);
+
+    void delete(T entity);
+}
