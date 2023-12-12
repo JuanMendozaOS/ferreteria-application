c902d4a302727175358199d174938f56a598840b
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-12-11 22:09:44 -0300

    Agregado módulo de TipoCliente

---
+++ src/main/java/py/edu/uaa/FerreteriaApplication/controller/TipoClienteController.java
@@ -0,0 +1,46 @@
+package py.edu.uaa.FerreteriaApplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import py.edu.uaa.FerreteriaApplication.model.TipoCliente;
+import py.edu.uaa.FerreteriaApplication.service.TipoClienteService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/tiposTipoCliente")
+public class TipoClienteController {
+    @Autowired
+    private TipoClienteService service;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<TipoCliente> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public TipoCliente findById(@PathVariable String id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TipoCliente create(@RequestBody TipoCliente entity) {
+        return service.create(entity);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public TipoCliente update(@RequestBody TipoCliente entity) {
+        return service.update(entity);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@RequestBody TipoCliente entity) {
+        service.delete(entity);
+    }
+}
---
+++ src/main/java/py/edu/uaa/FerreteriaApplication/service/TipoClienteService.java
@@ -0,0 +1,45 @@
+package py.edu.uaa.FerreteriaApplication.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import py.edu.uaa.FerreteriaApplication.app.exception.ResourceNotFoundException;
+import py.edu.uaa.FerreteriaApplication.app.model.IEntityService;
+import py.edu.uaa.FerreteriaApplication.model.TipoCliente;
+import py.edu.uaa.FerreteriaApplication.repository.TipoClienteRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TipoClienteService implements IEntityService<TipoCliente, String> {
+
+    @Autowired
+    private TipoClienteRepository repository;
+
+    @Override
+    public List<TipoCliente> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public TipoCliente findById(String id) {
+        Optional<TipoCliente> tipoClienteOptional;
+        tipoClienteOptional = repository.findById(id);
+        return tipoClienteOptional.orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @Override
+    public TipoCliente create(TipoCliente entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public TipoCliente update(TipoCliente entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public void delete(TipoCliente entity) {
+        repository.delete(entity);
+    }
+}
